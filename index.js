31a228bd3ac79507c57551b9d3905cfc2d8f04d5
Lales38 <128102923+Lales38@users.noreply.github.com>
2024-06-27 19:25:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import rutasProductos from './routes/rutas.productos.js';
+import rutasUsuarios from './routes/rutas.usuario.js'
+
+const app = express();
+const port = 3000;
+
+//Middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Rutas
+app.use('/', rutasProductos);
+app.use('/', rutasUsuarios);
+
+
+app.listen(port, () => {
+  console.log(`El servidor está corriendo en el puerto: ${port}, un lujo!!`);
+});

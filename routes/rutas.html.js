bc6c2b869733b745114a462307edb92bdfa05652
lales38 <lales38@hotmail.com>
2024-06-30 21:53:31 -0300

    actualizo ruta

--- routes/rutas.html.js
+++ routes/rutas.html.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 
 // Ruta para mostrar showProductos.html
-router.get('/showProductos', (req, res) => {
-  res.render('showProductos.html'); // Renderiza la vista showProductos.html
+router.get('/api/showProductos', (req, res) => {
+  res.render('showProductos'); // Renderiza la vista showProductos.html
 });
 
 export default router;

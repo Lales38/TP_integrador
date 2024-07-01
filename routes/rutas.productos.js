5562116a55ab10fb17caf46a754a0274a910d262
lales38 <lales38@hotmail.com>
2024-06-30 23:03:55 -0300

    actualizo5

--- routes/rutas.productos.js
+++ routes/rutas.productos.js
@@ -32,7 +32,7 @@ router.post("/api/producto/update/:id",upload.single("imagen"), upDateProductoPO
 router.post("/api/producto/borrar/:id", deleteProductoGET);
 
 router.get('/api/public/', (req, res) => {
-  res.render('showProductos.html'); // Renderiza la vista showProductos.html
+  res.sendFile(__dirname + '/views/showProductos.html');
 });
 
 export default router;
